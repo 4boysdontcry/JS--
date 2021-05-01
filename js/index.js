990b80685547a96c0f02db76c6a303d9f50a3503
4boysdontcry <hgy02166@gmail.com>
2021-05-01 22:11:38 +0900

    ../

--- js/index.js
+++ js/index.js
@@ -37,3 +37,18 @@ function onShowSubNavi(){
   $snav.eq(idx).show()
 }
 
+// Q1. 10까지의 숫자중에 짝수만 출력
+for(var i = 0; i < 11; i++){
+  if(i % 2 !== 0) {   // i값을 2로 나누었을때 나머지값(%)이 0과 같은가? -> 같으면(짝수) true, 다르면(홀수) false
+    continue;   // false의 경우 무시하고 진행
+  }
+  console.log(`q1. ${i}`)
+}
+
+// Q2. 8까지만 출력
+for(var i = 0; i < 11; i++){
+  if(i > 8) {
+    break;
+  }
+  console.log(`q2. ${i}`)
+}
