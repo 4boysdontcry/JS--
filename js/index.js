edaeec34abb426ab63f6fc79af7dd6a2e6e052a5
4boysdontcry <hgy02166@gmail.com>
2021-05-17 22:52:29 +0900

    bt-top 붙임

--- js/index.js
+++ js/index.js
@@ -55,11 +55,18 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
-
 // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
 var map = new kakao.maps.Map(mapContainer, mapOption); 
 
 
+
+// aside top 버튼 - 스르륵 올라가게 해줌
+$('.bt-top').click(movingTop);
+
+function movingTop(e){
+  e.preventDefault();		//서버로 전송을 막는다. 주소줄에 #안붙게 해줌
+	$('html, body').stop().animate({ "scrollTop": 0 }, 300);
+}
 
 
 
