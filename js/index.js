ad76abb4d4ee412a8c0e95b5d76e108a324dc48f
4boysdontcry <hgy02166@gmail.com>
2021-05-26 22:41:42 +0900

    es6 배운내용 추가

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
-document.write(1+1)
+// document.write(1+1)
 
-var avo = 3;
-avo >= 1 ?  console.log('buy 6 milk') : console.log('Dont Buy Milk')
+// 마트가서 우유사고 아보카도 있으면 여섯개 사와
+var avo = true;
+avo == true ? console.log('Buy 6 Milks') : console.log('buy milk')
 
-// $('.nd-btn').on('click', changeBg)
-// function changeBg(){
-//   $('body').css({"background-color": "black", "color": "white"})
-// }
 
+// 나이트 모드
 var nd = $('.nd-btn')
-
  nd.on('click', toggleBt)
  function toggleBt(){
    if(this.value == 'night'){
@@ -21,20 +18,6 @@ var nd = $('.nd-btn')
    }
  }
 
-
-// var i = 0;
-// var $nav = $('.navi')
-// var $navLen = $('.navi').length
-// var $snav = $('.sub-navi')
-// var $snavLen = $('.sub-navi').length
-
-// $nav.on('mouseenter', onShowSubNavi)
-
-// function onShowSubNavi(){
-//   var idx = $(this).data('idx')
-//   $snav.hide()
-//   $snav.eq(idx).show()
-// }
 
 // 각각의 navi 항목 안에 sub-navi가 위치하는 경우 class active를 이용해 show, hide 시킨다.
   $('.navi2').on('mouseenter', onShowSubNavi)
@@ -48,6 +31,8 @@ var nd = $('.nd-btn')
 		$('.sub-navi2').removeClass('active');
   }
 
+
+  // kakao API 지도붙이기
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = { 
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
@@ -82,13 +67,59 @@ for(var i = 0; i < 11; i++){
   }
   console.log(`q2. ${i}`)
 }
-var idx = 0;
-idx = $('.sub-navi').index();
-console.log($('.sub-navi'));
+console.log('====================')
 
 // ES6 - Arrow function
 const showResult = (a, b) => {
   console.log(a + b);
 }
 showResult(3, 5);
+
+
+// spread operator - arr와 arr2를 조각내서 arr3에 담아준다.
+const arr = ['A', 'B', 'c'];
+const arr2 = ['D', 'E', 'F'];
+
+const arr3 = [...arr, 'push', ...arr2];  // 중간에 문자열을 추가해줄 수도 있음
+console.log(arr3)
+
+
+// arguments(가변인자)
+function sum(){
+  let total = 0;
+  for(let v of arguments) total += v;
+  return total;
+}
+let numbers = [1, 2, 3, 4, 5, 6];   // numbers라는 배열이 argument에 순서대로 들어가면서 계속 더해진다. < 0+1=1+2=3+3=6+4=10+5=15+6=21 > 
+
+let total2 = sum(...numbers);
+console.log(total2);    // 21
+
+
+// 구조분해 할당
+const students = {
+  smart: [
+    {name: 'a', kor: 70},
+    {name: 'b', kor: 80},
+    {name: 'c', kor: 60}
+  ],
+  normal: [
+    {name: 'd', kor: 70},
+    {name: 'e', kor: 80},
+    {name: 'f', kor: 60}
+  ],
+  idiot: [
+    {name: 'g', kor: 70},
+    {name: 'h', kor: 80},
+    {name: 'i', kor: 60}
+  ]
+}
+
+let { smart: brillient, normal: gohard, idiot: poor } = students 
+// students라는 배열 안에 있는 배열들을 분리해서 각각의 이름에 맞는 let{}안의 변수에 할당한다.(이름이 같아야함)
+// 이때 그 이름 뒤에 :바꿀이름 을 적어주면 변수명을 바꿀수도 있다. ex) smart(students배열에 원래 있던 변수명): brillient(내가 바꿀 변수명)
+console.log(brillient)
+console.log(gohard)
+console.log(poor)
+
 
